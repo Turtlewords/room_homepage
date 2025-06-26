07e23042eb054ce7f02f46f0f97c63f15a8a8d47
Turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-06-26 14:34:17 -0500

    add mobile menu

--- app/js/index.js
+++ app/js/index.js
@@ -1,30 +1,74 @@
 // Elements
+const container = document.querySelector(".container");
 const carousel = document.querySelector(".carousel");
 const track = document.querySelector(".carousel__track");
+
 const slides = document.querySelectorAll(".carousel__slide");
+
 const prevBtn = document.querySelector(".carousel__button--left");
 const nextBtn = document.querySelector(".carousel__button--right");
+const heroMenuBtn = document.querySelector(".hero__menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenuBackground = document.querySelector(".mobile-menu__background");
+const closeMobileMenuBtn = document.querySelector(".mobile-menu__close-btn");
+
 const prevBtnList = document.querySelectorAll(".carousel__button--left");
 const nextBtnList = document.querySelectorAll(".carousel__button--right");
 
 const slideWidth = slides[0].getBoundingClientRect().width;
 
-
-
 slides.forEach(setSlidePosition);
-
 
 
 const mobileHeros = ["images/mobile-image-hero-1.jpg", "images/mobile-image-hero-2.jpg", 
     "images/mobile-image-hero-3.jpg"];
 
-    const desktopHeros = ["images/desktop-image-hero-1.jpg", "images?desktop-image-hero-2.jpg", 
+const desktopHeros = ["images/desktop-image-hero-1.jpg", "images?desktop-image-hero-2.jpg", 
     "images/desktop-image-hero-3.jpg"];
 
 
     // Event Listeners
 
+prevBtnList.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        let lastIndex = slides.length - 1;
+        const currentSlide = track.querySelector(".current-slide");
+        const prevSlide = currentSlide.previousElementSibling;
 
+    if (slides[0].classList.contains("current-slide")) {
+        let resetToMove = slides[lastIndex].style.left;
+        slides[0].classList.remove("current-slide")
+        slides[lastIndex].classList.add("current-slide")
+        track.style.transform = `translateX(-${resetToMove})`
+
+    } else {
+        moveToSlide(track, currentSlide, prevSlide)
+    }
+    })
+})
+
+
+nextBtnList.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        let lastIndex = slides.length - 1;
+        const currentSlide = track.querySelector(".current-slide");
+        const nextSlide = currentSlide.nextElementSibling;
+
+    if (slides[lastIndex].classList.contains("current-slide")) {
+        let resetToMove = slides[0].style.left;
+        slides[lastIndex].classList.remove("current-slide")
+        slides[0].classList.add("current-slide")
+        track.style.transform = `translateX(${resetToMove})`
+
+    } else {
+        moveToSlide(track, currentSlide, nextSlide)
+    }
+    })
+})
+
+heroMenuBtn.addEventListener("click", showMobileMenu);
+
+closeMobileMenuBtn.addEventListener("click", closeMobileMenu);
 
 
     // Function Calls
@@ -49,33 +93,26 @@ function positionArrowButtons() {
 }
 
 
-
 function moveToSlide(track, currentSlide, targetSlide) {
     
-        track.style.transform = `translateX(-${targetSlide.style.left})`
-        currentSlide.classList.remove("current-slide");
-        targetSlide.classList.add("current-slide");
+    track.style.transform = `translateX(-${targetSlide.style.left})`
+    currentSlide.classList.remove("current-slide");
+    targetSlide.classList.add("current-slide");
+}
+
+function showMobileMenu() {
+    mobileMenuBackground.style.display = "block";
+    mobileMenu.style.transform = "translateY(0)";
+}
+
+function closeMobileMenu() {
+    mobileMenuBackground.style.display = "none";
+    mobileMenu.style.transform = "translateY(-100%)";
 }
 
 // Add event listeners to all prev btns
 
-prevBtnList.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        let lastIndex = slides.length - 1;
-    const currentSlide = track.querySelector(".current-slide");
-    const prevSlide = currentSlide.previousElementSibling;
 
-    if (slides[0].classList.contains("current-slide")) {
-        let resetToMove = slides[lastIndex].style.left;
-        slides[0].classList.remove("current-slide")
-        slides[lastIndex].classList.add("current-slide")
-        track.style.transform = `translateX(-${resetToMove})`
-
-    } else {
-        moveToSlide(track, currentSlide, prevSlide)
-    }
-    })
-})
 
 // Single prev btn addeventlistener
 
@@ -98,23 +135,7 @@ prevBtnList.forEach((btn) => {
 
 // Add event listeners to all next btns
 
-nextBtnList.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        let lastIndex = slides.length - 1;
-        const currentSlide = track.querySelector(".current-slide");
-        const nextSlide = currentSlide.nextElementSibling;
 
-    if (slides[lastIndex].classList.contains("current-slide")) {
-        let resetToMove = slides[0].style.left;
-        slides[lastIndex].classList.remove("current-slide")
-        slides[0].classList.add("current-slide")
-        track.style.transform = `translateX(${resetToMove})`
-
-    } else {
-        moveToSlide(track, currentSlide, nextSlide)
-    }
-    })
-})
 
 // Add event listeners to one next button
 
